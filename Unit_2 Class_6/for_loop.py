0b56ab5b009e1d898483b15a82e50c5a6c56d120
Musfique Ahmed <anikmushfik@gmail.com>
2025-01-08 20:15:12 +0600

    class 6

---
+++ Unit_2 Class_6/for_loop.py
@@ -0,0 +1,45 @@
+# fruits = ["apple", "banana", "mango"]
+
+# for fruit in fruits: # for variable in list:
+#     print(fruit) #print(variable)
+
+
+# fruits = ["banana", "apple", "mango"]
+# print(fruits)
+# for fruit in fruits: #for variable name in lists:
+#     print(fruit) #code block
+
+# numbers = [1,2,3,4,5]
+# for number in numbers:
+#     print(number * 2)
+
+# brands = ["BMW", "Toyota", "Tesla", "AMG", "Audi", "Kia", "Nissan", "Cyber truck", "Tesla Type X", "Tesla Type Y"]
+
+# for brand in brands:
+#     print(brand)
+
+# colors = ["Red" ," Green" , "Blue"]
+# for color in colors:
+#     print(color)
+# friuts = ["1.Apple","2.Banana","3.Mango","4.Lamon"]
+
+# for friut in friuts:
+#     print(friut)
+
+# name = "Musfique"
+# for character in name:
+#     print(character)
+
+# name="hello"
+# for hi in name:
+#     print(hi)
+
+# Count the number of vowels in the word "education" using a for loop.
+# word  = "education"
+# vowel_count = 0
+
+# for chr in word:
+#     if chr in "aeiou":
+#         vowel_count = vowel_count + 1
+
+# print(vowel_count)
---
+++ Unit_2 Class_6/range.py
@@ -0,0 +1,58 @@
+# for i in range(0,5): #(o,5) = (5)
+#     print(i)
+
+# for i in range(3,7):
+#     print(i)
+
+# for i in range(11):
+#     print(i)
+
+# 
+# word = "education"
+# vowel_count = 0
+# for chr in word: 
+#     if chr in "aeiou":
+#         vowel_count = vowel_count + 1
+
+# print(vowel_count)
+
+# for i in range(1,100,10):
+#     print(i)
+
+# for i in range(2,21,2):
+#     print(i)
+
+# for i in range(2,21):
+#     if i % 2 == 0:# checks if i is even
+#         print(i)
+
+
+# nums = [1,2,3,4,5]
+# total = 0
+
+# for x in nums:  
+#     total += x # total = total + x
+
+# print(total)
+
+
+# bills = [233, 144, 567, 84, 100]
+# total_bill = 0
+
+# for bill in bills:
+#     total_bill += bill
+
+# print(total_bill)
+
+
+# products = ['apple', 'milk','oil']
+# bill = [120, 90, 530]
+
+# pro = input("Enter your product name: ")
+# bil = float(input("Enter your product bill: "))
+
+# products.append(pro)
+# bill.append(bil)
+
+# print(products)
+# print(bill)
