81bccc7fa538910e3aab37ae33674ed7303b13c1
Musfique Ahmed <anikmushfik@gmail.com>
2025-01-08 20:24:20 +0600

    Create nested.py

---
+++ Unit_2 Class_6/nested.py
@@ -0,0 +1,17 @@
+# matrix = [[1,2], [3,4], [5,6]]
+
+# for i in matrix: # i = [3,4]
+#     print(i)
+#     for j in i: #j =1 , j=2 
+#         print(j)
+
+# matrix = [[1,2], [3,4], [5,6]]
+# for i in matrix:
+#     for o in i:
+#         print(o)
+
+# matrix=[[3,4],[5,6],[7,8]]
+# for i in matrix:
+    
+#     for j in i:
+#         print(j)
