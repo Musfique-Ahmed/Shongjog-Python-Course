a1b77a6e65333347b8da29b4a6181a0ac8d5eb47
Musfique Ahmed <anikmushfik@gmail.com>
2025-01-11 18:47:50 +0600

    Create assignment_7.py

---
+++ Unit_2 Class_7/assignment_7.py
@@ -0,0 +1,17 @@
+#Task-5
+n = 5
+for i in range(1,11):
+    print(f"{n}*{i} = {n*i}")
+
+#Task-6
+user_input = "Python"
+for i in user_input[::-1]:
+    print(i, end="")
+
+#Task-7
+lst = [-10, 15, -20, 25, 30, -5]
+for i in lst:
+    if i >0:
+        print(i)
+
+# task-8
