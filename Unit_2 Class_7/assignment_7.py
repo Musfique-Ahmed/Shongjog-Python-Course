680f06699eb084d27ba1abfacc8a7cb96c44a82b
Musfique Ahmed <anikmushfik@gmail.com>
2025-01-11 19:21:47 +0600

    class 7

--- Unit_2 Class_7/assignment_7.py
+++ Unit_2 Class_7/assignment_7.py
@@ -11,7 +11,21 @@ for i in user_input[::-1]:
 #Task-7
 lst = [-10, 15, -20, 25, 30, -5]
 for i in lst:
-    if i >0:
+    if i > 0: 
         print(i)
 
 # task-8
+n= 5
+fact = 1
+for i in range(2,n+1):
+    fact = fact * i
+print(fact)
+
+#task-9:
+lst = [3, 8, 2, 9, 4, 7]
+lasrge = lst[0] #9
+for i in lst: #3,8,
+    if i > lasrge:#3>3, 8>3? i=8
+        lasrge = i
+
+print(lasrge)
---
+++ Unit_2 Class_7/count.py
@@ -0,0 +1,10 @@
+count = 0 #1,2,3,4
+
+while count<5:
+    print(count)
+    count += 1 # count = count + 1
+
+n = 1
+while n <11:
+    print(n)
+    n += 1
