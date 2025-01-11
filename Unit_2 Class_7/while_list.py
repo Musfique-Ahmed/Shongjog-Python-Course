006ce9f872509e4bd9156416501f06cd6f377d06
Musfique Ahmed <anikmushfik@gmail.com>
2025-01-11 22:13:39 +0600

    Update while_list.py

--- Unit_2 Class_7/while_list.py
+++ Unit_2 Class_7/while_list.py
@@ -1,31 +1,31 @@
-# unconfired = ["Musayab", "Manha", "tafsir","Nillim"]
+# # unconfired = ["Musayab", "Manha", "tafsir","Nillim"]
 
-# confirmed = []
+# # confirmed = []
 
-# while unconfired:
-#     name = unconfired.pop()
-#     confirmed.append(name)
-#     # print("hi")
+# # while unconfired:
+# #     name = unconfired.pop()
+# #     confirmed.append(name)
+# #     # print("hi")
 
-# print(unconfired, confirmed)
+# # print(unconfired, confirmed)
 
-# # fav_movies = [gsrht,shmhbs,sg] 
-# # seen = []
+# # # fav_movies = [gsrht,shmhbs,sg] 
+# # # seen = []
 
-# # print("😂")
+# # # print("😂")
 
-# count = 0
-# while count<5:
-#     print("👍")
-#     count+=1
+# # count = 0
+# # while count<5:
+# #     print("👍")
+# #     count+=1
 
-pets = ['cat','cat', 'dog', 'dog']
-while "cat" in pets:
-    pets.remove("cat")
+# pets = ['cat','cat', 'dog', 'dog']
+# while "cat" in pets:
+#     pets.remove("cat")
 
-print(pets)
+# print(pets)
 
-unliked_food = ['korola', 'lau', 'milk' , 'korola', 'korola', 'milk']
-while "korola" in unliked_food:
-    unliked_food.remove("korola")
-print(unliked_food)
+# unliked_food = ['korola', 'lau', 'milk' , 'korola', 'korola', 'milk']
+# while "korola" in unliked_food:
+#     unliked_food.remove("korola")
+# print(unliked_food)
