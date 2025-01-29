21c86f2fff678d87ad5fd3cf474175cb22648c52
Musfique Ahmed <mahmed2330101@bsds.uiu.ac.bd>
2025-01-29 20:34:58 +0600

    class 3

--- Unit_3 Class_3/bank.py
+++ Unit_3 Class_3/bank.py
@@ -1,19 +1,37 @@
 # Bank Account
-class Account:
+class CustomError(Exception):
+    """Exception raised for custom error scenarios."""
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+        
+    def __str__(self):
+        return f"{self.message}"
+    
+class BankAccount:
     def __init__(self, name, ac_no, balance = 500):
         self.name = name
         self.ac_no = ac_no
         self.__balance = balance # Private variale
 
     def show_balance(self): #getter method
-        print(self.__balance)
+        print(f"Balance: {self.__balance}")
 
     def deposit(self, amount):
         self.__balance += amount
         print(f"{amount}BDT was deposited to your account!")
 
+    def withdraw(self, amount):
+        
+        if (self.__balance - amount) < 500:
+            raise CustomError("You cannot withdraw this amount, You don't have enough Money!")
+        else:
+            self.__balance -= amount
+            print(f"{amount} BDT was withdrawed from your account")
 
-ac_1 = Account("Musfique", 23470, 10000)
+
+
+ac_1 = BankAccount("Musfique", 23470, 10000)
 # print(ac_1.balance)
 print(ac_1.name)
 print(ac_1.ac_no)
@@ -23,7 +41,10 @@ ac_1.show_balance()
 # print(ac_1.balance)
 ac_1.deposit(5000)
 ac_1.show_balance()
-
+ac_1.withdraw(14000)
+ac_1.show_balance()
+ac_1.withdraw(600)
+ac_1.show_balance()
 """
 Create a "Student" class with a private variable __grade. 
 Add a method get_grade() to return the grade safely.
--- Unit_3 Class_3/student.py
+++ Unit_3 Class_3/student.py
@@ -40,3 +40,119 @@ s_1 = Student("Manha","O-",15, "2750136768", "A", "A+")
 s_1.display_info()
 s_1.get_grade()
 s_1.donate_blood(3)
+
+
+
+
+class Person:
+    def __init__(self, name, age, address):
+        self.name, self.age, self.address = name, age, address
+
+    def greeting(self):
+        return f"Hello, my name is {self.name}."
+
+    def __str__(self):
+        return f"{self.name}, {self.age} years old, Address: {self.address}"
+
+class Student(Person):
+    def __init__(self, name, age, address, grade, major):
+        super().__init__(name, age, address)
+        self.grade, self.major = grade, major
+
+    def greeting(self):
+        return f"Hello, I'm {self.name}, a {self.major} student."
+
+    def is_passed(self):
+        return self.grade >= 60
+
+    def __str__(self):
+        return f"{super().__str__()}, Major: {self.major}, Grade: {self.grade}, Passed: {'Yes' if self.is_passed() else 'No'}"
+
+# Example usage:
+student = Student("Manha Binta Bellah", 11, "Mohammadpur,Dhaka", 100, "Game developer and Software Ingeneer")
+print(student.greeting())  # Output: Hello, I'm Manha Binta Bellah, a Game developer and Software Ingenee
+
+
+
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def introduce(self):
+        return f"Hi, my name is {self.name} . I'm {self.age} years old."
+
+class Student(Person):
+    def __init__(self, name, age, student_id):
+        super().__init__(name, age)
+        self.student_id = student_id
+
+    def introduce(self):
+        return f"{super().introduce()} I am a student. My ID is{self.student_id}."
+
+person = Person("Raisa", 13)
+print(person.introduce())
+
+print(person.introduce())  
+
+student = Student("Emma", 20, "S92945")
+print(student.introduce())
+
+
+
+
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def introduce(self):
+        return f"My name is {self.name}, and I am {self.age} years old."
+
+class Student(Person):
+    def __init__(self, name, age, grade):
+        super().__init__(name, age)
+        self.grade = grade
+    def display_info(self):
+        print(f'''
+Name: {self.name}
+age: {self.age}
+grade: {self.grade}
+                ''')
+
+    # def get_grade(self):
+    #     return f"{self.name} is in grade {self.grade}."
+
+student = Student("Ahan", 17, 10)
+# print(student.introduce())  # Output: My name is Ahan, and I am 17 years old.
+print(student.display_info())  # Output: Ahan is in grade 10.
+
+
+
+
+class Person:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+    def display_info(self):
+        print(f"I am {self.name} and I am {self.age} years old.")
+
+class Student(Person):
+    def __init__(self, name, age, hobby):
+        super().__init__(name, age)
+        self.hobby = hobby
+
+    def display_info(self):
+        print(f"I am {self.name} and I am {self.age} years old. My hobby is {self.hobby}")
+
+    def get_grade(self):
+        return f"{self.name}'s Hobby is {self.hobby}."
+
+Per_1 = Person("Anik",23)
+Per_1.display_info()
+Stu_1 = Student("Erik",12,"playing")
+Stu_1.display_info()
+print(Stu_1.get_grade())
