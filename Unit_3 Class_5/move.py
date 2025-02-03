eddb302a94aa6e6ee381a6efae32f5a7a9cd4b77
Musfique Ahmed <mahmed2330101@bsds.uiu.ac.bd>
2025-02-03 20:24:44 +0600

    Create move.py

---
+++ Unit_3 Class_5/move.py
@@ -0,0 +1,41 @@
+import pygame
+
+#Initialize Pygame
+pygame.init()
+
+##Window
+screen = pygame.display.set_mode((1000, 800)) #Screen size
+pygame.display.set_caption("My Shapes") #Title
+
+# Colors
+WHITE = (255,255,255)
+RED = (255,0,0)
+BLUE = (0,0,255)
+GREEN = (0, 255, 0)
+
+x, y = 200, 300
+
+# Game Loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        x -= 1
+    if keys[pygame.K_RIGHT]:
+        x += 1
+    if keys[pygame.K_UP]:
+        x -= 1
+    if keys[pygame.K_DOWN]:
+        x += 1
+    
+    screen.fill(WHITE)
+    pygame.draw.circle(screen, BLUE, (x, y), 30)
+
+    pygame.display.update()
+
+# Quit
+pygame.quit()
