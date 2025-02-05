f7ac6f8d7cfb31e4849eaab7553a1eb518311d48
Musfique Ahmed <mahmed2330101@bsds.uiu.ac.bd>
2025-02-05 20:44:25 +0600

    calss 6

--- Unit_3 Class_6/APPs/main.py
+++ Unit_3 Class_6/APPs/main.py
@@ -4,4 +4,9 @@ st.title("Welcome to Streamlit!")
 st.write("This is a sample web app!")
 
 name = st.text_input("Enter your name: ")
+num = st.number_input("enter numebr")
+st.selectbox("Choose", ["A","b" , "C"])
 st.write("Hello", name)
+
+st.button("Click me")
+st.file_uploader("Upload File")
---
+++ Unit_3 Class_6/APPs/my_port.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+st.sidebar.title("Navigation")
+page = st.sidebar.radio("GO to", ["Home", "Projects", "Skills", "Contact"])
+
+
+
+if page == "Home":
+    st.title("Welcome to my Portfolio!")
+    # st.image
+    st.write("Hi i am Musfique Ahmed, a data Science Student at UIU.")
+    st.write("This portfolio showcases my works")
+
+elif page == "Projects":
+    st.title("🤯 My Projects!")
+
+    st.subheader("Monkey Pox detector")
+    st.write("this project is an ml pproject to detect monkey pox!")
+    st.markdown("[GitHubLink](https://github.com/Musfique-Ahmed/monkeypox-perdition-app)")
+    st.write("---")
+
+
+elif page== "Contact":
+    st.title("MY contact")
+    st.write("Email: anbdgshbh@gmail.com")
+    st.write("[GitHub](https://github.com/Musfique-Ahmed)")
